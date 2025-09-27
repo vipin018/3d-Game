67143037f514c77b3ef63ee7910648b2833aab22
Vipin Pathak <vipinpathak018@gmail.com>
2025-09-27 13:30:30 +0530

    added fullscreen toggle functionality on double-click event in events.js

--- src/events.js
+++ src/events.js
@@ -2,8 +2,19 @@
 import { onWindowResize } from './window-resize.js';
 import { onKeyDown, onKeyUp } from './controls.js';
 
+function toggleFullscreen() {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        }
+    }
+}
+
 export function initEvents() {
     window.addEventListener('resize', onWindowResize);
     document.addEventListener('keydown', onKeyDown);
     document.addEventListener('keyup', onKeyUp);
+    window.addEventListener('dblclick', toggleFullscreen);
 }
